74e5424932599e53004a7a563ae788e952e6991a
Darshan M <darshanmukunda098@gmail.com>
2023-03-22 23:39:50 +0530

    add Q16

---
+++ Question 016_What are the different types of exceptions/DemoException.java
@@ -0,0 +1,12 @@
+import java.io.FileReader;
+
+public class DemoException {
+    public static void main(String[] args) {
+        try{
+            //FileReader f = new FileReader("");
+
+        }finally {
+            System.out.println("inside finally block");
+        }
+    }
+}
