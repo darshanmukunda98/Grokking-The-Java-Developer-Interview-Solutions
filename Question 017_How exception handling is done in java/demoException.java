3cb77a523d5a6000e81addddc96cd9eb82f0382e
Darshan M <darshanmukunda098@gmail.com>
2023-03-22 23:44:42 +0530

    add Q17

---
+++ Question 017_How exception handling is done in java/demoException.java
@@ -0,0 +1,9 @@
+import java.io.FileReader;
+
+class demoException {
+    public static void main(String[] args) {
+        /*try{
+            FileReader f = new FileReader("");
+        }*/
+    }
+}
